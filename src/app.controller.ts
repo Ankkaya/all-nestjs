eb76edad11037b5170446b68ef78c4d8e4dce7e6
Ankkaya <staryou@126.com>
2023-12-25 16:08:29 +0800

    多种 provider，灵活注入对象

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,39 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  // 构造器注入，指定类
+  // constructor(private readonly appService: AppService) {}
+
+  // 属性注入
+  // @Inject(AppService)
+  // private readonly appService: AppService
+
+  // token 是字符串，注入时候需要手动指定注入对象的 token
+  // constructor(@Inject('app_service') private readonly appService: AppService) {}
+
+  // 获取通过 useValue 注入的对象
+  // constructor(
+  //   @Inject('app_service') private readonly appService: AppService,
+  //   @Inject('person') private readonly person: { name: string; age: number },
+  // ) {}
+
+  // 获取 useFactory 动态注入对象
+  constructor(
+    @Inject('app_service') private readonly appService: AppService,
+    @Inject('person') private readonly person: { name: string; age: number },
+    @Inject('random') private readonly random: number,
+    @Inject('async') private readonly async: { name: string; desc: string }, // 异步注入
+    @Inject('alias') private readonly alias: { name: string; desc: string }, // 别名
+  ) {}
 
   @Get()
   getHello(): string {
+    console.log(this.alias);
+    console.log(this.random);
+    console.log(this.async);
+    console.log(this.person);
     return this.appService.getHello();
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,64 @@ import { PersonModule } from './person/person.module';
 @Module({
   imports: [XxxModule, PersonModule],
   controllers: [AppController],
-  providers: [AppService],
+  // AppService 是 class 也是 token
+  // providers: [AppService],
+  // 字符串指定 token，useClass 指定 class
+
+  providers: [
+    AppService,
+    // 通过参数注入其他 provider
+    {
+      provide: 'person3',
+      useFactory(person: { name: string }, appService: AppService) {
+        return {
+          name: person.name,
+          desc: appService.getHello(),
+        };
+      },
+      // 声明两个 token， 一个是字符串 token 的 person，一个是类 token 的 AppService
+      // 上面的 person 和 appService 注入
+      inject: ['person', AppService],
+    },
+    {
+      provide: 'app_service',
+      useClass: AppService,
+    },
+    // 注入指定值
+    {
+      provide: 'person',
+      useValue: {
+        name: 'ankkaya',
+        age: 20,
+      },
+    },
+    // 动态提供 provide 的值
+    {
+      provide: 'random',
+      useFactory: () => {
+        return Math.random();
+      },
+    },
+    // 异步操作
+    {
+      provide: 'async',
+      useFactory: async () => {
+        await new Promise((resolve) => {
+          setTimeout(() => {
+            resolve('async');
+          }, 2000);
+        });
+        return {
+          name: 'async',
+          desc: 'async',
+        };
+      },
+    },
+    // 设置别名
+    {
+      provide: 'alias',
+      useExisting: 'person',
+    },
+  ],
 })
 export class AppModule {}

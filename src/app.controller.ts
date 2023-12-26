37c615bbb0779b466b10d2c9a20f591c378d6716
Ankkaya <staryou@126.com>
2023-12-26 17:26:08 +0800

    切换不同上下文

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseFilters, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AaaFilter } from './aaa.filter';
+import { AaaException } from './AaaException';
+import { AaaGuard } from './aaa.guard';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @UseFilters(AaaFilter)
+  @UseGuards(AaaGuard)
   getHello(): string {
+    throw new AaaException('aaa', 'bbb');
     return this.appService.getHello();
   }
 }

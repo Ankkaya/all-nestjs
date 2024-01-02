0c5c045648bfea5f6262084bc66d3e757b73cf61
Ankkaya <staryou@126.com>
2024-01-02 14:45:07 +0800

    module和provider的循环依赖处理

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { XxxModule } from './xxx/xxx.module';
 import { PersonModule } from './person/person.module';
+import { AaaModule } from './aaa/aaa.module';
+import { BbbModule } from './bbb/bbb.module';
+import { CccService } from './ccc.service';
+import { DddService } from './ddd.service';
 
 @Module({
-  imports: [XxxModule, PersonModule],
+  imports: [XxxModule, PersonModule, AaaModule, BbbModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, CccService, DddService],
 })
 export class AppModule {}
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { DddService } from './ddd.service';
+import { CccService } from './ccc.service';
 
 @Injectable()
 export class AppService {
+  constructor(
+    private dddService: DddService,
+    private cccService: CccService,
+  ) {}
+
   getHello(): string {
-    return 'Hello World!';
+    return this.dddService.ddd() + this.cccService.ccc();
   }
 }

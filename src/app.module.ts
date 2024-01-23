91321573cf1e7246e6d646274d58f6c3ae7bfab3
Ankkaya <staryou@126.com>
2024-01-23 11:29:45 +0800

    在 Nest 中操作 Redis

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,26 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { XxxModule } from './xxx/xxx.module';
 import { PersonModule } from './person/person.module';
+import { createClient } from 'redis';
 
 @Module({
   imports: [XxxModule, PersonModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: 'REDIS_CLIENT',
+      async useFactory() {
+        const client = createClient({
+          socket: {
+            host: 'localhost',
+            port: 6380,
+          },
+        });
+        await client.connect();
+        return client;
+      },
+    },
+  ],
 })
 export class AppModule {}

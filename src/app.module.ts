50fa1db39ef20d3a761ea1cf3fee1ca30c72bba5
Ankkaya <staryou@126.com>
2024-01-30 18:14:52 +0800

    基于ACL实现权限控制

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,39 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { XxxModule } from './xxx/xxx.module';
 import { PersonModule } from './person/person.module';
+import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Permission } from './user/entities/permission.entity';
+import { User } from './user/entities/user.entity';
+import { AaaModule } from './aaa/aaa.module';
+import { BbbModule } from './bbb/bbb.module';
+import { RedisModule } from './redis/redis.module';
 
 @Module({
-  imports: [XxxModule, PersonModule],
+  imports: [
+    XxxModule,
+    PersonModule,
+    UserModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3307,
+      username: 'root',
+      password: 'root',
+      database: 'acl_test',
+      synchronize: true,
+      logging: true,
+      entities: [User, Permission],
+      poolSize: 10,
+      connectorPackage: 'mysql2',
+      extra: {
+        authPlugin: 'sha256_password',
+      },
+    }),
+    AaaModule,
+    BbbModule,
+    RedisModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import * as session from 'express-session';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.use(
+    session({
+      secret: 'guang',
+      resave: false,
+      saveUninitialized: false,
+    }),
+  );
   app.useStaticAssets('public', { prefix: '/static' });
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
 bootstrap();

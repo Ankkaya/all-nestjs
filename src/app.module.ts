de346aacd603eadd063368e36034f299c1489796
Ankkaya <staryou@126.com>
2024-02-06 16:13:07 +0800

    基于 RBAC 实现权限控制

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,60 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { XxxModule } from './xxx/xxx.module';
 import { PersonModule } from './person/person.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+import { User } from './user/entities/user.entity';
+import { Role } from './user/entities/role.entity';
+import { Permission } from './user/entities/permission.entity';
+import { JwtModule } from '@nestjs/jwt';
+import { AaaModule } from './aaa/aaa.module';
+import { BbbModule } from './bbb/bbb.module';
+import { LoginGuard } from 'src/login.guard';
+import { APP_GUARD } from '@nestjs/core';
+import { PermissionGuard } from './permission.guard';
 
 @Module({
-  imports: [XxxModule, PersonModule],
+  imports: [
+    XxxModule,
+    PersonModule,
+    JwtModule.register({
+      global: true,
+      secret: 'ankkaya',
+      signOptions: {
+        expiresIn: '1d',
+      },
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3307,
+      username: 'root',
+      password: 'root',
+      database: 'rbac_test',
+      synchronize: true,
+      logging: true,
+      entities: [User, Role, Permission],
+      poolSize: 10,
+      connectorPackage: 'mysql2',
+      extra: {
+        authPlugin: 'sha256_password',
+      },
+    }),
+    UserModule,
+    AaaModule,
+    BbbModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: LoginGuard,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: PermissionGuard,
+    },
+  ],
 })
 export class AppModule {}

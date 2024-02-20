45e0b16f487f7cd89b4274775085dc5c833a8cba
Ankkaya <staryou@126.com>
2024-02-20 09:49:49 +0800

    动态读取不同环境的配置

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,25 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { XxxModule } from './xxx/xxx.module';
 import { PersonModule } from './person/person.module';
+import { ConfigModule } from '@nestjs/config';
+import { BbbModule } from './bbb/bbb.module';
+import * as path from 'path';
+import config2 from 'config2';
 
 @Module({
-  imports: [XxxModule, PersonModule],
+  imports: [
+    XxxModule,
+    PersonModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: [
+        path.join(process.cwd(), '.aaa.env'),
+        path.join(process.cwd(), '.env'),
+      ],
+      load: [config2],
+    }),
+    BbbModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

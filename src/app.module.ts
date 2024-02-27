03f8da5f696b022abb1ec2e03796e4f73c6e2788
Ankkaya <staryou@126.com>
2024-02-27 14:38:59 +0800

    chapter_69:Redis 实现分布式 session

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { XxxModule } from './xxx/xxx.module';
 import { PersonModule } from './person/person.module';
+import { RedisModule } from './redis/redis.module';
+import { SessionModule } from './session/session.module';
 
 @Module({
-  imports: [XxxModule, PersonModule],
+  imports: [XxxModule, PersonModule, RedisModule, SessionModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useStaticAssets('public', { prefix: '/static' });
+  app.use(cookieParser());
   await app.listen(3000);
 }
 bootstrap();

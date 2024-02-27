fa1c2d4461d421e3db5773df6f5ab84b2f635717
Ankkaya <staryou@126.com>
2024-02-27 09:13:15 +0800

    chapter_66: Nginx 的 2 大核心用法

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,8 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.setGlobalPrefix('/api');
   app.useStaticAssets('public', { prefix: '/static' });
-  await app.listen(3000);
+  await app.listen(3002);
 }
 bootstrap();

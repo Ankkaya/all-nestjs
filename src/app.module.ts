d64f057545fb6d231720e0d314a0f14f07aa991c
Ankkaya <staryou@126.com>
2024-03-04 17:36:21 +0800

    Nest 的 3 种定时任务

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,61 @@
-import { Module } from '@nestjs/common';
+import { Inject, Module, OnApplicationBootstrap } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { XxxModule } from './xxx/xxx.module';
 import { PersonModule } from './person/person.module';
+import { ScheduleModule, SchedulerRegistry } from '@nestjs/schedule';
+import { TaskService } from './task.service';
+import { AaaModule } from './aaa/aaa.module';
+import { CronJob } from 'cron';
 
 @Module({
-  imports: [XxxModule, PersonModule],
+  imports: [XxxModule, PersonModule, ScheduleModule.forRoot(), AaaModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, TaskService],
 })
-export class AppModule {}
+export class AppModule implements OnApplicationBootstrap {
+  @Inject(SchedulerRegistry)
+  private schedulerRegistry: SchedulerRegistry;
+
+  onApplicationBootstrap() {
+    const crons = this.schedulerRegistry.getCronJobs();
+    crons.forEach((item, key) => {
+      item.stop();
+      this.schedulerRegistry.deleteCronJob(key);
+    });
+
+    const intervals = this.schedulerRegistry.getIntervals();
+    intervals.forEach((item) => {
+      const interval = this.schedulerRegistry.getInterval(item);
+      clearInterval(interval);
+
+      this.schedulerRegistry.deleteInterval(item);
+    });
+
+    const timeouts = this.schedulerRegistry.getTimeouts();
+    timeouts.forEach((item) => {
+      const timeout = this.schedulerRegistry.getTimeout(item);
+      clearTimeout(timeout);
+
+      this.schedulerRegistry.deleteTimeout(item);
+    });
+
+    console.log('onApplicationBootstrap', crons, intervals, timeouts);
+
+    const job = new CronJob(`*/5 * * * * *`, () => {
+      console.log('cron job');
+    });
+    this.schedulerRegistry.addCronJob('job1', job);
+    job.start();
+
+    const interval = setInterval(() => {
+      console.log('interval job');
+    }, 3000);
+    this.schedulerRegistry.addInterval('job2', interval);
+
+    const timeout = setTimeout(() => {
+      console.log('timeout job');
+    }, 5000);
+    this.schedulerRegistry.addTimeout('job3', timeout);
+  }
+}

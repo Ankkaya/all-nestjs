644b5eda37ff95d387e1b2a7e0a709c94ee1a064
Ankkaya <staryou@126.com>
2024-03-04 16:51:23 +0800

    定时任务＋redis 实现阅读量计数

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import * as session from 'express-session';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useStaticAssets('public', { prefix: '/static' });
+  app.use(
+    session({
+      secret: 'ankkaya',
+      resave: false,
+      saveUninitialized: false,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();

7917e7143c594e39f63241e3296c62dba70c3620
Ankkaya <staryou@126.com>
2024-03-06 16:09:42 +0800

    短链服务

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,36 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { XxxModule } from './xxx/xxx.module';
 import { PersonModule } from './person/person.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UniqueCode } from './entities/UniqueCode';
+import { UniqueCodeService } from './unique-code.service';
+import { ScheduleModule } from '@nestjs/schedule';
+import { ShortLongMap } from './entities/ShortLongMap';
+import { ShortLongMapService } from './short-long-map.service';
 
 @Module({
-  imports: [XxxModule, PersonModule],
+  imports: [
+    ScheduleModule.forRoot(),
+    XxxModule,
+    PersonModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3307,
+      username: 'root',
+      password: 'root',
+      database: 'short_url',
+      synchronize: true,
+      logging: true,
+      entities: [UniqueCode, ShortLongMap],
+      poolSize: 10,
+      connectorPackage: 'mysql2',
+      extra: {
+        authPlugin: 'sha256_password',
+      },
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, UniqueCodeService, ShortLongMapService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useStaticAssets('public', { prefix: '/static' });
+  // 允许跨域
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();

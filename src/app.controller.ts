9910f4e176fd4a938463dfd0f045d8b0baa899c4
Ankkaya <staryou@126.com>
2024-03-08 14:12:42 +0800

    sharp 压缩 gif

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  BadRequestException,
+  Controller,
+  Get,
+  ParseIntPipe,
+  Post,
+  Query,
+  Res,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
 import { AppService } from './app.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { existsSync } from 'fs';
+import * as sharp from 'sharp';
+import { Response } from 'express';
 
 @Controller()
 export class AppController {
@@ -8,5 +22,42 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('upload')
+  @UseInterceptors(
+    FileInterceptor('file', {
+      dest: 'uploads',
+    }),
+  )
+  uploadFile(@UploadedFile() file: Express.Multer.File) {
+    console.log('file', file);
+  }
+
+  @Get('compression')
+  async compression(
+    @Query('path') filePath: string,
+    @Query('color', ParseIntPipe) color: number,
+    @Res() res: Response,
+  ) {
+    console.log('filePath', filePath);
+    console.log('color', color);
+
+    if (!existsSync(filePath)) {
+      throw new BadRequestException('File not found');
+    }
+
+    const data = await sharp(filePath, {
+      animated: true,
+      limitInputPixels: false,
+    })
+      .gif({
+        colours: color,
+      })
+      .toBuffer();
+
+    res.set('Content-Disposition', `attachment; filename="dest.gif"`);
+    res.send(data);
+    // return 'success';
   }
 }
